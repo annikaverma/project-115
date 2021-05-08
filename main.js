0e21014d26535e5384f490bcb2b94e021d262e48
annikaverma <76493450+annikaverma@users.noreply.github.com>
2021-05-08 18:30:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x-40;
+    noseY = results[0].pose.nose.y;
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clown_nose, noseX, noseY, 80, 35);
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
